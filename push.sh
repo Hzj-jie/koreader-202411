163be292b37751bb4efdb29020b176607b9a3912
hzj_jie <hzj_jie@hotmail.com>
2024-12-29 10:57:03 -0800

    clean before push

--- push.sh
+++ push.sh
@@ -5,5 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+./clean.sh
+
 rsync -acv --no-o --no-g --exclude='.*.un~' extensions/ root@192.168.1.$1:/mnt/us/extensions/
 rsync -acv --no-o --no-g --exclude='.*.un~' koreader/ root@192.168.1.$1:/mnt/us/koreader/
