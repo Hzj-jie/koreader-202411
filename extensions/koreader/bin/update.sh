f40e20559443a22f7d32a3b3d40ec3b161e90073
hzj_jie <hzj_jie@hotmail.com>
2024-12-29 10:17:00 -0800

    copy updates to the right folders

--- extensions/koreader/bin/update.sh
+++ extensions/koreader/bin/update.sh
@@ -2,7 +2,7 @@
 
 pushd /mnt/us/
 wget -O - https://github.com/Hzj-jie/koreader-202411/archive/master.tar.gz | tar xz
-cp -rl koreader-202411-master/koreader/ /mnt/us/koreader/
-cp -rl koreader-202411-master/extensions/koreader/ /mnt/us/extensions/koreader/
+cp -rl koreader-202411-master/koreader/ /mnt/us/
+cp -rl koreader-202411-master/extensions/koreader/ /mnt/us/extensions/
 rm -rf koreader-202411-master
 popd
