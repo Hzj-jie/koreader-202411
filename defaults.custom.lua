6010533ff3ef9c260550400b7f0db0d7139c0916
hzj_jie <hzj_jie@hotmail.com>
2024-12-29 10:52:05 -0800

    a defaults.custom.lua not in the koreader folder

---
+++ defaults.custom.lua
@@ -0,0 +1,5 @@
+-- This file is not used on all the voyage units.
+
+return {
+  KINDLE_AMBIENT_BRIGHTNESS_MULTIPLIER = 2,
+}
---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo Need an address
+  exit 1
+fi
+
+rsync -acv --no-o --no-g --exclude='.*.un~' extensions/ root@192.168.1.$1:/mnt/us/extensions/
+rsync -acv --no-o --no-g --exclude='.*.un~' koreader/ root@192.168.1.$1:/mnt/us/koreader/
