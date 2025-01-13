246d746010fc1d5aa919cf194daf296a887aec38
hzj_jie <hzj_jie@hotmail.com>
2025-01-12 22:49:58 -0800

    remove kosync config

--- settings.reader.lua
+++ settings.reader.lua
@@ -208,12 +208,9 @@ return {
     ["kosync"] = {
         ["auto_sync"] = true,
         ["checksum_method"] = 0,
-        ["custom_server"] = "https://hzj-jie-ubuntu.westus.cloudapp.azure.com:7200",
         ["pages_before_update"] = 5,
         ["sync_backward"] = 1,
         ["sync_forward"] = 2,
-        ["userkey"] = "daa613d5e3c54ac8f088066018276724",
-        ["username"] = "Hzj_jie",
     },
     ["low_battery_threshold"] = 10,
     ["low_pan_rate"] = true,
