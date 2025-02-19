dddc147176a2068d9b20e5a51a8523e2e58c79d1
Zijie He <hzj_jie@hotmail.com>
2025-02-19 11:04:41 -0800

    install-deps.sh

---
+++ install-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This command is copied from README.md and should only be necessary when
+# building native binaries.
+
+sudo apt-get install autoconf automake build-essential ca-certificates cmake \
+     gcc-multilib gettext git libsdl2-2.0-0 libtool libtool-bin lua-busted \
+     lua5.1 luarocks meson nasm ninja-build patch perl pkg-config unzip wget
