1a2a620df0390c8c2c008e6d33f539b14c230e79
Hzj_jie <hzj_jie@hotmail.com>
2025-02-19 23:25:04 -0800

    update diff-baseline.sh

--- diff-baseline.sh
+++ diff-baseline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git diff "$@" d7848ef99a53e99c9377d9d45f81cbd510fb4584 ':(exclude)koreader/data/dict' ':(exclude)screensaver' ':(exclude)koreader/fonts' ':(exclude)koreader/plugins/calculator.koplugin' ':(exclude)linux' ':(exclude)legacy' ':(exclude)extensions' ':(exclude)origin' ':(exclude)koreader/data/cr3.ini' | grep ''
+git diff "$@" d7848ef99a53e99c9377d9d45f81cbd510fb4584 koreader/ ':(exclude)koreader/data/dict' ':(exclude)koreader/README.md' ':(exclude)koreader/jit/vmdef.lua' ':(exclude)koreader/fonts' ':(exclude)koreader/settings.reader.lua' ':(exclude)koreader/settings' ':(exclude)koreader/data/cr3.ini' | grep ''
 git diff "$@" 231e6bd36634077930a8957d1656a83b2e022550 f29ec9e7e8c3b264a44d5c8dd42536a85d31df07 | grep ''
