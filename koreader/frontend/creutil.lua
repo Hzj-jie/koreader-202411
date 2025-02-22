22f821be6a98390b2265f558da7971ca5bc9fbad
Hzj_jie <hzj_jie@hotmail.com>
2025-02-21 20:23:44 -0800

    should use screen dpi / user config for cre font size

--- koreader/frontend/creutil.lua
+++ koreader/frontend/creutil.lua
@@ -5,7 +5,7 @@ local CreUtil = {}
 function CreUtil.font_size(value)
   -- CRE does not respect the DPI setting in koreader, the font size needs to be
   -- adjusted to match the real size.
-  return value * (Device:getDeviceScreenDPI() or Device.screen:getDPI()) / 160
+  return value * Device.screen:getDPI() / 160
 end
 
 return CreUtil
