1c7a007a7295063cbc8d39fa7e38979e71237b4e
Hzj_jie <hzj_jie@hotmail.com>
2025-03-14 23:58:12 -0700

    avoid simpletcpserver crashing

--- koreader/frontend/ui/message/simpletcpserver.lua
+++ koreader/frontend/ui/message/simpletcpserver.lua
@@ -24,8 +24,12 @@ end
 
 function SimpleTCPServer:start()
     self.server = socket.bind(self.host, self.port)
-    self.server:settimeout(0.01) -- set timeout (10ms)
-    logger.dbg("SimpleTCPServer: Server listening on port " .. self.port)
+    if self.server then
+        self.server:settimeout(0.01) -- set timeout (10ms)
+        logger.dbg("SimpleTCPServer: Server listening on port " .. self.port)
+    else
+        logger.warn("Failed to start SimpleTCPServer on port " .. self.port)
+    end
 end
 
 function SimpleTCPServer:stop()
@@ -34,28 +38,27 @@ end
 
 function SimpleTCPServer:waitEvent()
     local client = self.server:accept() -- wait for a client to connect
-    if client then
-        -- We expect to get all headers in 100ms. We will block during this timeframe.
-        client:settimeout(0.1, "t")
-        local lines = {}
-        while true do
-            local data = client:receive("*l") -- read a line from input
-            if not data then -- timeout
-                client:close()
-                break
-            end
-            if data == "" then -- proper empty line after request headers
-                table.insert(lines, data) -- keep it in content
-                data = table.concat(lines, "\r\n")
-                logger.dbg("SimpleTCPServer: Received data: ", data)
-                -- Give us more time to process the request and send the response
-                client:settimeout(0.5, "t")
-                self.receiveCallback(data, client)
-                    -- This should call SimpleTCPServer:send() to send
-                    -- the response and close this connection.
-            else
-                table.insert(lines, data)
-            end
+    if not client then return end
+    -- We expect to get all headers in 100ms. We will block during this timeframe.
+    client:settimeout(0.1, "t")
+    local lines = {}
+    while true do
+        local data = client:receive("*l") -- read a line from input
+        if not data then -- timeout
+            client:close()
+            break
+        end
+        if data == "" then -- proper empty line after request headers
+            table.insert(lines, data) -- keep it in content
+            data = table.concat(lines, "\r\n")
+            logger.dbg("SimpleTCPServer: Received data: ", data)
+            -- Give us more time to process the request and send the response
+            client:settimeout(0.5, "t")
+            self.receiveCallback(data, client)
+                -- This should call SimpleTCPServer:send() to send
+                -- the response and close this connection.
+        else
+            table.insert(lines, data)
         end
     end
 end
