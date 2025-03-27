d1999fdadc595842508bbe81768491fae12fd2a9
Hzj_jie <hzj_jie@hotmail.com>
2025-03-26 23:17:57 -0700

    skip background runner onSuspend

--- koreader/plugins/backgroundrunner.koplugin/main.lua
+++ koreader/plugins/backgroundrunner.koplugin/main.lua
@@ -4,7 +4,7 @@ local Device = require("device")
 -- it is also unused by other plugins.
 -- See https://github.com/koreader/koreader/issues/6297
 if Device:isAndroid() then
-    return { disabled = true, }
+  return { disabled = true, }
 end
 
 local CommandRunner = require("commandrunner")
@@ -78,205 +78,204 @@ local _ = require("gettext")
 -- (All of them in the monotonic time scale, like the main event loop & task queue).
 
 local BackgroundRunner = {
-    jobs = PluginShare.backgroundJobs,
-    running = false,
+  jobs = PluginShare.backgroundJobs,
+  running = false,
 }
 
 --- Copies required fields from |job|.
 -- @return a new table with required fields of a valid job.
 function BackgroundRunner:_clone(job)
-    assert(job ~= nil)
-    local result = {}
-    result.when = job.when
-    result.repeated = job.repeated
-    result.executable = job.executable
-    result.callback = job.callback
-    result.environment = job.environment
-    return result
+  assert(job ~= nil)
+  local result = {}
+  result.when = job.when
+  result.repeated = job.repeated
+  result.executable = job.executable
+  result.callback = job.callback
+  result.environment = job.environment
+  return result
 end
 
 function BackgroundRunner:_shouldRepeat(job)
-    if type(job.repeated) == "nil" then return false end
-    if type(job.repeated) == "boolean" then return job.repeated end
-    if type(job.repeated) == "function" then
-        local status, result = pcall(job.repeated)
-        if status then
-            return result
-        else
-            return false
-        end
+  if type(job.repeated) == "nil" then return false end
+  if type(job.repeated) == "boolean" then return job.repeated end
+  if type(job.repeated) == "function" then
+    local status, result = pcall(job.repeated)
+    if status then
+      return result
     end
-    if type(job.repeated) == "number" then
-        job.repeated = job.repeated - 1
-        return job.repeated > 0
-    end
-
     return false
+  end
+  if type(job.repeated) == "number" then
+    job.repeated = job.repeated - 1
+    return job.repeated > 0
+  end
+
+  return false
 end
 
 function BackgroundRunner:_finishJob(job)
-    if type(job.executable) == "function" then
-        local time_diff = job.end_time - job.start_time
-        local threshold = time.s(1)
-        job.timeout = (time_diff > threshold)
-    end
-    job.blocked = job.timeout
-    if not job.blocked and self:_shouldRepeat(job) then
-        self:_insert(self:_clone(job))
-    end
-    if type(job.callback) == "function" then
-        pcall(job.callback)
-    end
+  if type(job.executable) == "function" then
+    local time_diff = job.end_time - job.start_time
+    local threshold = time.s(1)
+    job.timeout = (time_diff > threshold)
+  end
+  job.blocked = job.timeout
+  if not job.blocked and self:_shouldRepeat(job) then
+    self:_insert(self:_clone(job))
+  end
+  if type(job.callback) == "function" then
+    pcall(job.callback)
+  end
 end
 
 --- Executes |job|.
 -- @treturn boolean true if job is valid.
 function BackgroundRunner:_executeJob(job)
-    assert(not CommandRunner:pending())
-    if job == nil then return false end
-    if job.executable == nil then return false end
+  assert(not CommandRunner:pending())
+  if job == nil then return false end
+  if job.executable == nil then return false end
 
-    if type(job.executable) == "string" then
-        CommandRunner:start(job)
-        return true
+  if type(job.executable) == "string" then
+    CommandRunner:start(job)
+    return true
+  end
+  if type(job.executable) == "function" then
+    job.start_time = UIManager:getTime()
+    local status, err = pcall(job.executable)
+    if status then
+      job.result = 0
+    else
+      logger.warn("BackgroundRunner: _executeJob [", job.executable, "] failed, ", err)
+      job.result = 1
+      job.exception = err
     end
-    if type(job.executable) == "function" then
-        job.start_time = UIManager:getTime()
-        local status, err = pcall(job.executable)
-        if status then
-            job.result = 0
-        else
-            logger.warn("BackgroundRunner: _executeJob [", job.executable, "] failed, ", err)
-            job.result = 1
-            job.exception = err
-        end
-        job.end_time = time.now()
-        self:_finishJob(job)
-        return true
-    end
-    return false
+    job.end_time = time.now()
+    self:_finishJob(job)
+    return true
+  end
+  return false
 end
 
 --- Polls the status of the pending CommandRunner.
 function BackgroundRunner:_poll()
-    assert(CommandRunner:pending())
-    local result = CommandRunner:poll()
-    if result == nil then return end
+  assert(CommandRunner:pending())
+  local result = CommandRunner:poll()
+  if result == nil then return end
 
-    self:_finishJob(result)
+  self:_finishJob(result)
 end
 
 function BackgroundRunner:_execute()
-    logger.dbg("BackgroundRunner: _execute() @ ", os.time())
-    if CommandRunner:pending() then
-        self:_poll()
-    else
-        local round = 0
-        while #self.jobs > 0 do
-            local job = table.remove(self.jobs, 1)
-            if job.insert_time == nil then
-                -- Jobs are first inserted to jobs table from external users.
-                -- So they may not have an insert field.
-                job.insert_time = UIManager:getTime()
-            end
-            local should_execute = false
-            local should_ignore = false
-            if type(job.when) == "function" then
-                local status, result = pcall(job.when)
-                if status then
-                    should_execute = result
-                else
-                    should_ignore = true
-                end
-            elseif type(job.when) == "number" then
-                if job.when >= 0 then
-                    should_execute = (UIManager:getTime() - job.insert_time >= time.s(job.when))
-                else
-                    should_ignore = true
-                end
-            elseif type(job.when) == "string" then
-                --- @todo (Hzj_jie): Implement "idle" mode
-                if job.when == "best-effort" then
-                    should_execute = (round > 0)
-                elseif job.when == "idle" then
-                    should_execute = (round > 1)
-                else
-                    should_ignore = true
-                end
-            else
-                should_ignore = true
-            end
-
-            if should_execute then
-                logger.dbg("BackgroundRunner: run job ", job, " @ ", os.time())
-                assert(not should_ignore)
-                self:_executeJob(job)
-                break
-            elseif not should_ignore then
-                table.insert(self.jobs, job)
-            end
-
-            round = round + 1
-            if round > 2 then break end
-        end
-    end
-
-    self.running = false
-    if PluginShare.stopBackgroundRunner == nil then
-        if #self.jobs == 0 and not CommandRunner:pending() then
-            logger.dbg("BackgroundRunnerWidget: no job, stop running @ ", os.time())
+  logger.dbg("BackgroundRunner: _execute() @ ", os.time())
+  if PluginShare.stopBackgroundRunner == true then
+    logger.dbg("BackgroundRunnerWidget: skip running @ ", os.time())
+    return
+  end
+  if CommandRunner:pending() then
+    self:_poll()
+  else
+    local round = 0
+    while #self.jobs > 0 do
+      local job = table.remove(self.jobs, 1)
+      if job.insert_time == nil then
+        -- Jobs are first inserted to jobs table from external users.
+        -- So they may not have an insert field.
+        job.insert_time = UIManager:getTime()
+      end
+      local should_execute = false
+      local should_ignore = false
+      if type(job.when) == "function" then
+        local status, result = pcall(job.when)
+        if status then
+          should_execute = result
         else
-            self:_schedule()
+          should_ignore = true
         end
-    else
-        logger.dbg("BackgroundRunnerWidget: stop running @ ", os.time())
+      elseif type(job.when) == "number" then
+        if job.when >= 0 then
+          should_execute = (UIManager:getTime() - job.insert_time >= time.s(job.when))
+        else
+          should_ignore = true
+        end
+      elseif type(job.when) == "string" then
+        --- @todo (Hzj_jie): Implement "idle" mode
+        if job.when == "best-effort" then
+          should_execute = (round > 0)
+        elseif job.when == "idle" then
+          should_execute = (round > 1)
+        else
+          should_ignore = true
+        end
+      else
+        should_ignore = true
+      end
+
+      if should_execute then
+        logger.dbg("BackgroundRunner: run job ", job, " @ ", os.time())
+        assert(not should_ignore)
+        self:_executeJob(job)
+        break
+      elseif not should_ignore then
+        table.insert(self.jobs, job)
+      end
+
+      round = round + 1
+      if round > 2 then break end
     end
+  end
+
+  self.running = false
+  if #self.jobs == 0 and not CommandRunner:pending() then
+    logger.dbg("BackgroundRunnerWidget: no job, stop running @ ", os.time())
+  else
+    self:_schedule()
+  end
 end
 
 function BackgroundRunner:_schedule()
-    if self.running == false then
-        if #self.jobs == 0 and not CommandRunner:pending() then
-            logger.dbg("BackgroundRunnerWidget: no job, not running @ ", os.time())
-        else
-            logger.dbg("BackgroundRunnerWidget: start running @ ", os.time())
-            self.running = true
-            UIManager:scheduleIn(2, function() self:_execute() end)
-        end
+  if self.running == false then
+    if #self.jobs == 0 and not CommandRunner:pending() then
+      logger.dbg("BackgroundRunnerWidget: no job, not running @ ", os.time())
     else
-        logger.dbg("BackgroundRunnerWidget: a schedule is pending @ ",
-                   os.time())
+      logger.dbg("BackgroundRunnerWidget: start running @ ", os.time())
+      self.running = true
+      UIManager:scheduleIn(2, function() self:_execute() end)
     end
+  else
+    logger.dbg("BackgroundRunnerWidget: a schedule is pending @ ",
+           os.time())
+  end
 end
 
 function BackgroundRunner:_insert(job)
-    job.insert_time = UIManager:getTime()
-    table.insert(self.jobs, job)
+  job.insert_time = UIManager:getTime()
+  table.insert(self.jobs, job)
 end
 
 local BackgroundRunnerWidget = WidgetContainer:extend{
-    name = "backgroundrunner",
-    runner = BackgroundRunner,
+  name = "backgroundrunner",
+  runner = BackgroundRunner,
 }
 
 function BackgroundRunnerWidget:init()
-    BackgroundRunner:_schedule()
+  BackgroundRunner:_schedule()
 end
 
 function BackgroundRunnerWidget:onSuspend()
-    logger.dbg("BackgroundRunnerWidget:onSuspend() @ ", os.time())
-    PluginShare.stopBackgroundRunner = true
+  logger.dbg("BackgroundRunnerWidget:onSuspend() @ ", os.time())
+  PluginShare.stopBackgroundRunner = true
 end
 
 function BackgroundRunnerWidget:onResume()
-    logger.dbg("BackgroundRunnerWidget:onResume() @ ", os.time())
-    PluginShare.stopBackgroundRunner = nil
-    BackgroundRunner:_schedule()
+  logger.dbg("BackgroundRunnerWidget:onResume() @ ", os.time())
+  PluginShare.stopBackgroundRunner = nil
+  BackgroundRunner:_schedule()
 end
 
 function BackgroundRunnerWidget:onBackgroundJobsUpdated()
-    logger.dbg("BackgroundRunnerWidget:onBackgroundJobsUpdated() @ ", os.time())
-    PluginShare.stopBackgroundRunner = nil
-    BackgroundRunner:_schedule()
+  logger.dbg("BackgroundRunnerWidget:onBackgroundJobsUpdated() @ ", os.time())
+  PluginShare.stopBackgroundRunner = nil
+  BackgroundRunner:_schedule()
 end
 
 return BackgroundRunnerWidget
