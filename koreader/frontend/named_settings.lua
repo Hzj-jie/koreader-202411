cd7fc85bcc370bf5c56c6e997e72e79a1bf0da6c
Hzj_jie <hzj_jie@hotmail.com>
2025-03-31 20:55:03 -0700

    typo in named_settings comment

--- koreader/frontend/named_settings.lua
+++ koreader/frontend/named_settings.lua
@@ -1,7 +1,7 @@
 --- Provides functions to read settings from G_reader_settings with default
 --- values. This file helps share the configurations across multiple components
 --- without needing to rely on the order of saveSetting and readSetting calls,
---- as well as the preexistent of the value in the G_require("named_settings").
+--- as well as the preexistent of the value in the G_named_settings.
 --- Unless there is a clear reason, functions won't return nil or empty values.
 
 local named_settings = {
