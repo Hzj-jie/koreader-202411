596f0f245599ca56aa44db68cbbdf76f4a83989a
Hzj_jie <hzj_jie@hotmail.com>
2025-04-10 20:13:21 -0700

    repeat wifi setters

--- koreader/frontend/device/kindle/device.lua
+++ koreader/frontend/device/kindle/device.lua
@@ -196,17 +196,21 @@ local function kindleEnableWifi(toggle)
   if toggle == nil then toggle = 0 end
   assert(type(toggle) == "number")
   local lipc = LibLipcs:of("com.github.koreader.networkmgr")
-  if LibLipcs:isFake(lipc) then
-    -- No liblipclua on FW < 5.x ;)
-    -- Always kill 3G first...
-    os.execute("lipc-set-prop -i com.lab126.wan enable 0")
-    os.execute("lipc-set-prop -i com.lab126.cmd wirelessEnable " .. toggle)
-    os.execute("lipc-set-prop -i com.lab126.wifid enable " .. toggle)
-  else
-    -- Be extremely thorough... c.f., #6019
-    -- NOTE: I *assume* this'll also ensure we prefer Wi-Fi over 3G/4G, which is a plus in my book...
-    lipc:set_int_property("com.lab126.cmd", "wirelessEnable", toggle)
-    lipc:set_int_property("com.lab126.wifid", "enable", toggle)
+  -- I think the order of calling these two setters matter, so repeating the
+  -- same commands twice to ensure either can be done before the other.
+  for _ = 0, 1 do
+    if LibLipcs:isFake(lipc) then
+      -- No liblipclua on FW < 5.x ;)
+      -- Always kill 3G first...
+      os.execute("lipc-set-prop -i com.lab126.wan enable 0")
+      os.execute("lipc-set-prop -i com.lab126.cmd wirelessEnable " .. toggle)
+      os.execute("lipc-set-prop -i com.lab126.wifid enable " .. toggle)
+    else
+      -- Be extremely thorough... c.f., #6019
+      -- NOTE: I *assume* this'll also ensure we prefer Wi-Fi over 3G/4G, which is a plus in my book...
+      lipc:set_int_property("com.lab126.cmd", "wirelessEnable", toggle)
+      lipc:set_int_property("com.lab126.wifid", "enable", toggle)
+    end
   end
 end
 
