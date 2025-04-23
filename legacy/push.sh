84578c32f1bd382687df9366d7bf9f6aa264d450
Hzj_jie <hzj_jie@hotmail.com>
2025-04-22 23:26:58 -0700

    Add legacy/push.sh

---
+++ legacy/push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf /media/hzj_jie/Kindle/koreader.old/
+mv /media/hzj_jie/Kindle/koreader /media/hzj_jie/Kindle/koreader.old
+cp -rL . /media/hzj_jie/Kindle/koreader/
+cp -r --update=none /media/hzj_jie/Kindle/koreader.old/* /media/hzj_jie/Kindle/koreader/
