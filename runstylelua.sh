8f99d972346fa0c9cb326198a4a6ddae68c3d928
Hzj_jie <hzj_jie@hotmail.com>
2025-05-03 20:13:17 -0700

    Run runstylelua.sh twice to avoid syntax breakages

--- runstylelua.sh
+++ runstylelua.sh
@@ -4,3 +4,8 @@
 # https://github.com/JohnnyMorganz/StyLua/releases/download/v2.1.0/stylua-linux-x86_64.zip
 ./stylua koreader/
 ./stylua --syntax Lua53 koreader/jit/dump.lua
+
+./stylua koreader/
+./stylua --syntax Lua53 koreader/jit/dump.lua
+
+# Run everything twice to avoid syntax breakages.
