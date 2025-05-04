f25a10d909e355c9725b3ebfce9daa26f3ebb430
Hzj_jie <hzj_jie@hotmail.com>
2025-05-03 19:52:22 -0700

    run sstylua on jit/dump.lua differently

--- runstylelua.sh
+++ runstylelua.sh
@@ -3,3 +3,4 @@
 # stylua is downloaded from
 # https://github.com/JohnnyMorganz/StyLua/releases/download/v2.1.0/stylua-linux-x86_64.zip
 ./stylua koreader/
+./stylua --syntax Lua53 koreader/jit/dump.lua
