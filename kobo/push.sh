8925ea368459deca1aa783cb4ecccf1ad2116be8
Hzj_jie <hzj_jie@hotmail.com>
2025-05-04 23:18:00 -0700

    force rsync-path in push.sh

--- kobo/push.sh
+++ kobo/push.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rsync -acvLK --no-o --no-g --exclude=lns.sh . root@192.168.1.16:/mnt/onboard/.adds/koreader
+export KOREADER_DIR=/mnt/onboard/.adds/koreader
+rsync --rsync-path=$KOREADER_DIR -acvLK --no-o --no-g --exclude=lns.sh . root@192.168.1.16:$KOREADER_DIR
