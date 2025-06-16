1b51427185f56e6da32d91102318751cc38b5a39
Hzj_jie <hzj_jie@hotmail.com>
2025-06-15 21:07:08 -0700

    log more details in BackgroundJobs.insert

--- koreader/frontend/background_jobs.lua
+++ koreader/frontend/background_jobs.lua
@@ -5,18 +5,21 @@ Helper to manage background jobs.
 BackgroundJobs = {}
 
 function BackgroundJobs.insert(job)
+  local jobs = require("pluginshare").backgroundJobs
   -- Do not require modules until it's really necessary.
   -- Note, the BackgroundJobs may be required before other modules.
+  -- Slightly perf optimization to avoid dumping job unnecessarily.
   if G_defaults:isTrue("DEV_MODE") then
-    local logger = require("logger")
     require("logger").info(
       "new background job ",
       require("dump")(job),
+      ", in total ",
+      #jobs + 1,
       " at ",
       debug.traceback()
     )
   end
-  table.insert(require("pluginshare").backgroundJobs, job)
+  table.insert(jobs, job)
   -- Raises an event to avoid depending on BackgroundRunner plugin directly.
   require("ui/uimanager"):broadcastEvent(
     require("ui/event"):new("BackgroundJobsUpdated")
