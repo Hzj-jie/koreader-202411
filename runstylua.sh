bad6e615fe35937cc1d5ddccdac2ae57bed2ef9b
zijiehe-google-com <zijiehe@google.com>
2025-06-20 11:09:34 -0700

    Update runstylua to explain --verify

--- runstylua.sh
+++ runstylua.sh
@@ -4,6 +4,11 @@
 # https://github.com/JohnnyMorganz/StyLua/releases/download/v2.1.0/stylua-linux-x86_64.zip
 
 function run {
+  # --verify crashes stylua.
+  # thread '<unnamed>' panicked at src/verify_ast.rs:148:39:
+  # internal error: entered unreachable code
+  # note: run with `RUST_BACKTRACE=1` environment variable to display a
+  # backtrace
   ./stylua "$1"
   if [ -e "$1/jit/dump.lua" ]; then
     ./stylua --syntax Lua53 "$1/jit/dump.lua"
