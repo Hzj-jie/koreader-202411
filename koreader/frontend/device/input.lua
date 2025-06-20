7ec05df50075ce7a3abe584681ae44df8a2a544f
Hzj_jie <hzj_jie@hotmail.com>
2025-06-20 15:25:25 -0700

    Revert "Do not trigger Kindle/PowerD:afterResume - onResume until ExitingSS"
    This reverts commit 6f1da634c18bebde3232d700725fefda6152010d.

--- koreader/frontend/device/input.lua
+++ koreader/frontend/device/input.lua
@@ -468,7 +468,8 @@ function Input:init()
   -- set up fake event map
   self.event_map[10000] = "IntoSS" -- Requested to go into screen saver
   self.event_map[10001] = "OutOfSS" -- Requested to go out of screen saver
-  self.event_map[10002] = "ExitingSS" -- Specific to Kindle, SS *actually* closed
+  -- Unused yet.
+  -- self.event_map[10002] = "ExitingSS" -- Specific to Kindle, SS *actually* closed
   self.event_map[10010] = "UsbPlugIn"
   self.event_map[10011] = "UsbPlugOut"
   self.event_map[10020] = "Charging"
