539b4fcdff44a57892403595b58304be1af12b1b
zijiehe-google-com <zijiehe@google.com>
2025-06-20 11:06:40 -0700

    Update runstylua.sh to reduce dup and run in origin/frontend.stylua

--- runstylua.sh
+++ runstylua.sh
@@ -2,10 +2,19 @@
 
 # stylua is downloaded from
 # https://github.com/JohnnyMorganz/StyLua/releases/download/v2.1.0/stylua-linux-x86_64.zip
-./stylua koreader/
-./stylua --syntax Lua53 koreader/jit/dump.lua
 
-./stylua koreader/
-./stylua --syntax Lua53 koreader/jit/dump.lua
+function run {
+  ./stylua "$1"
+  if [ -e "$1/jit/dump.lua" ]; then
+    ./stylua --syntax Lua53 "$1/jit/dump.lua"
+  fi
+}
+
+function run_twice {
+  run "$@"
+}
+
+run_twice "koreader"
+run_twice "origin/frontend.stylua"
 
 # Run everything twice to avoid syntax breakages.
