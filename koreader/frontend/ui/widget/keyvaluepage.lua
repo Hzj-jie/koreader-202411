937a0f84bd58229d77bf29744f7db25153ab7865
Zijie He <hzj_jie@hotmail.com>
2025-07-18 10:54:30 -0700

    Revert "disallow keyvaluepage propagating the events to the widgets below"
    This reverts commit 3e118a34e6fd0a6178483a7e25bd822cedab05ec.
    
    Other widgets below the KeyValuePage won't be triggered by the tap, I
    assume this should be a problem of the ButtonTable or InputDialog.

--- koreader/frontend/ui/widget/keyvaluepage.lua
+++ koreader/frontend/ui/widget/keyvaluepage.lua
@@ -303,8 +303,6 @@ local KeyValuePage = FocusManager:extend({
   title_bar_left_icon = nil,
   title_bar_left_icon_tap_callback = nil,
   title_bar_left_icon_hold_callback = nil,
-  -- Disallow other widgets behind to hijack the events.
-  stop_events_propagation = true,
 })
 
 function KeyValuePage:init()
