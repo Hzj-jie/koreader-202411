e9601afcbfe7115324835c0a2eefbf752cc91a28
Hzj_jie <hzj_jie@hotmail.com>
2025-07-19 22:38:11 -0700

    rm tar.gz files in ffi

--- ffi/make.sh
+++ ffi/make.sh
@@ -4,6 +4,7 @@ readonly LIBFFI_VERSION=3.4.5
 rm -rf libffi-$LIBFFI_VERSION libffi-$LIBFFI_VERSION.tar.gz
 wget https://github.com/libffi/libffi/releases/download/v$LIBFFI_VERSION/libffi-$LIBFFI_VERSION.tar.gz
 tar xzf libffi-$LIBFFI_VERSION.tar.gz
+rm libffi-$LIBFFI_VERSION.tar.gz
 pushd libffi-$LIBFFI_VERSION
 ./configure
 make
@@ -13,6 +14,7 @@ readonly LUA_FFI_VERSION=1.1.0
 rm -rf lua-ffi-$LUA_FFI_VERSION lua-ffi-$LUA_FFI_VERSION.tar.gz
 wget https://github.com/zhaojh329/lua-ffi/releases/download/v$LUA_FFI_VERSION/lua-ffi-$LUA_FFI_VERSION.tar.gz
 tar xzf lua-ffi-$LUA_FFI_VERSION.tar.gz
+rm lua-ffi-$LUA_FFI_VERSION.tar.gz
 pushd lua-ffi-$LUA_FFI_VERSION
 mkdir build
 pushd build
