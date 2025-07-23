ec377a3506184f9800e81766a9a15b6f457f3fe8
Hzj_jie <hzj_jie@hotmail.com>
2025-07-22 23:23:37 -0700

    Allow libkoreader-lfs.lua loading libs/lfs.so

--- linux/libs/libkoreader-lfs.lua
+++ linux/libs/libkoreader-lfs.lua
@@ -1,7 +1,7 @@
-ok, koreader_lfs = pcall(package.loadlib, "libs/libkoreader-lfs.so", "luaopen_lfs")
-if ok then
+local ok, koreader_lfs = pcall(package.loadlib, "libs/libkoreader-lfs.so", "luaopen_lfs")
+if ok and koreader_lfs ~= nil then
   print("libkoreader-lfs.so is used")
   return koreader_lfs()
 end
 print("lfs.so is used")
-return require("libs/lfs")
+return package.loadlib("libs/lfs.so", "luaopen_lfs")()
