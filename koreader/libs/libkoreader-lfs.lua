1b2a71dd055ce2b547a9df976f2e43ea30f47388
Hzj_jie <hzj_jie@hotmail.com>
2025-07-23 18:03:32 -0700

    Stop printing which lfs is used

--- koreader/libs/libkoreader-lfs.lua
+++ koreader/libs/libkoreader-lfs.lua
@@ -1,7 +1,5 @@
 local ok, koreader_lfs = pcall(package.loadlib, "libs/libkoreader-lfs.so", "luaopen_lfs")
 if ok and koreader_lfs ~= nil then
-  print("libkoreader-lfs.so is used")
   return koreader_lfs()
 end
-print("lfs.so is used")
 return package.loadlib("libs/lfs.so", "luaopen_lfs")()
