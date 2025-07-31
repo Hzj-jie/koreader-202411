e504dfde4dfd80300cbec604025616f263ec62ed
Hzj_jie <hzj_jie@hotmail.com>
2025-07-31 00:32:39 -0700

    posix_h_ffi.lua, does not work

--- koreader/ffi/posix_h_ffi.lua
+++ koreader/ffi/posix_h_ffi.lua
@@ -1,15 +1,6 @@
 local ffi = require("ffi")
 
 ffi.cdef([[
-static const int EPERM = 1;
-static const int EINTR = 4;
-static const int EAGAIN = 11;
-static const int EINVAL = 22;
-static const int ENODEV = 19;
-static const int ENOSYS = 38;
-static const int EPIPE = 32;
-static const int ETIME = 62;
-static const int ETIMEDOUT = 110;
 struct timezone {
   int tz_minuteswest;
   int tz_dsttime;
@@ -18,26 +9,6 @@ int pipe(int *) __attribute__((nothrow, leaf));
 int fork(void) __attribute__((nothrow));
 int dup(int) __attribute__((nothrow, leaf));
 int dup2(int, int) __attribute__((nothrow, leaf));
-static const int O_APPEND = 1024;
-static const int O_CREAT = 64;
-static const int O_TRUNC = 512;
-static const int O_RDWR = 2;
-static const int O_RDONLY = 0;
-static const int O_WRONLY = 1;
-static const int O_NONBLOCK = 2048;
-static const int O_CLOEXEC = 524288;
-static const int S_IRUSR = 256;
-static const int S_IWUSR = 128;
-static const int S_IXUSR = 64;
-static const int S_IRWXU = 448;
-static const int S_IRGRP = 32;
-static const int S_IWGRP = 16;
-static const int S_IXGRP = 8;
-static const int S_IRWXG = 56;
-static const int S_IROTH = 4;
-static const int S_IWOTH = 2;
-static const int S_IXOTH = 1;
-static const int S_IRWXO = 7;
 int open(const char *, int, ...);
 int mq_open(const char *, int, ...) __attribute__((nothrow, leaf));
 ssize_t mq_receive(int, char *, size_t, unsigned int *);
@@ -51,7 +22,6 @@ int execvp(const char *, char *const *) __attribute__((nothrow, leaf));
 ssize_t write(int, const void *, size_t);
 ssize_t read(int, void *, size_t);
 int kill(int, int) __attribute__((nothrow, leaf));
-static const int WNOHANG = 1;
 int waitpid(int, int *, int);
 int getpid(void) __attribute__((nothrow, leaf));
 int getppid(void) __attribute__((nothrow, leaf));
@@ -61,17 +31,7 @@ struct pollfd {
   short int events;
   short int revents;
 };
-static const int POLLIN = 1;
-static const int POLLOUT = 4;
-static const int POLLERR = 8;
-static const int POLLHUP = 16;
 int poll(struct pollfd *, long unsigned int, int);
-static const int PROT_READ = 1;
-static const int PROT_WRITE = 2;
-static const int MAP_SHARED = 1;
-static const int MAP_ANONYMOUS = 32;
-static const int MAP_FAILED = -1;
-static const int PATH_MAX = 4096;
 int memcmp(const void *, const void *, size_t) __attribute__((pure, leaf, nothrow));
 void *mmap(void *, size_t, int, int, int, off_t) __attribute__((nothrow, leaf));
 int munmap(void *, size_t) __attribute__((nothrow, leaf));
@@ -89,7 +49,6 @@ typedef int clockid_t;
 int clock_getres(clockid_t, struct timespec *) __attribute__((nothrow, leaf));
 int clock_gettime(clockid_t, struct timespec *) __attribute__((nothrow, leaf));
 int clock_settime(clockid_t, const struct timespec *) __attribute__((nothrow, leaf));
-static const int TIMER_ABSTIME = 1;
 int clock_nanosleep(clockid_t, int, const struct timespec *, struct timespec *);
 void *malloc(size_t) __attribute__((malloc, leaf, nothrow));
 void *calloc(size_t, size_t) __attribute__((malloc, leaf, nothrow));
@@ -100,7 +59,6 @@ char *strndup(const char *, size_t) __attribute__((malloc, leaf, nothrow));
 int strcoll(const char *, const char *) __attribute__((nothrow, leaf, pure));
 int strcmp(const char *, const char *) __attribute__((pure, leaf, nothrow));
 int strcasecmp(const char *, const char *) __attribute__((pure, leaf, nothrow));
-static const int F_OK = 0;
 int access(const char *, int) __attribute__((nothrow, leaf));
 typedef struct _IO_FILE FILE;
 typedef long long unsigned int dev_t;
@@ -165,12 +123,6 @@ typedef int pid_t;
 struct sched_param {
   int sched_priority;
 };
-static const int SCHED_OTHER = 0;
-static const int SCHED_BATCH = 3;
-static const int SCHED_IDLE = 5;
-static const int SCHED_FIFO = 1;
-static const int SCHED_RR = 2;
-static const int SCHED_RESET_ON_FORK = 1073741824;
 int sched_getscheduler(int) __attribute__((nothrow, leaf));
 int sched_setscheduler(int, int, const struct sched_param *) __attribute__((nothrow, leaf));
 int sched_getparam(int, struct sched_param *) __attribute__((nothrow, leaf));
@@ -197,10 +149,7 @@ struct ifaddrs {
   } ifa_ifu;
   void *ifa_data;
 };
-static const int NI_MAXHOST = 1025;
 int getifaddrs(struct ifaddrs **) __attribute__((nothrow, leaf));
-static const int AF_INET = 2;
-static const int AF_INET6 = 10;
 int getnameinfo(const struct sockaddr *restrict, unsigned int, char *restrict, unsigned int, char *restrict, unsigned int, int);
 struct in_addr {
   unsigned int s_addr;
@@ -225,18 +174,9 @@ struct sockaddr_in6 {
   struct in6_addr sin6_addr;
   uint32_t sin6_scope_id;
 };
-static const int NI_NUMERICHOST = 1;
 const char *gai_strerror(int) __attribute__((nothrow, leaf));
 void freeifaddrs(struct ifaddrs *) __attribute__((nothrow, leaf));
 int socket(int, int, int) __attribute__((nothrow, leaf));
-static const int PF_INET = 2;
-static const int SOCK_DGRAM = 2;
-static const int SOCK_RAW = 3;
-static const int SOCK_NONBLOCK = 2048;
-static const int SOCK_CLOEXEC = 524288;
-static const int IPPROTO_IP = 0;
-static const int IPPROTO_ICMP = 1;
-static const int IFNAMSIZ = 16;
 struct ifmap {
   long unsigned int mem_start;
   long unsigned int mem_end;
@@ -264,11 +204,6 @@ struct ifreq {
     char *ifru_data;
   } ifr_ifru;
 };
-static const int SIOCGIFHWADDR = 35111;
-static const int RTF_UP = 1;
-static const int RTF_GATEWAY = 2;
-static const int IFF_UP = 1;
-static const int IFF_LOOPBACK = 8;
 struct iw_point {
   void *pointer;
   short unsigned int length;
@@ -318,11 +253,7 @@ struct iwreq {
   } ifr_ifrn;
   union iwreq_data u;
 };
-static const int SIOCGIWNAME = 35585;
-static const int SIOCGIWESSID = 35611;
 typedef char *caddr_t;
-static const int IW_ESSID_MAX_SIZE = 32;
-static const int IW_ENCODE_INDEX = 255;
 typedef unsigned int socklen_t;
 struct icmphdr {
   uint8_t type;
@@ -396,9 +327,6 @@ struct icmp {
     uint8_t id_data[1];
   } icmp_dun;
 };
-static const int ICMP_MINLEN = 8;
-static const int ICMP_ECHO = 8;
-static const int ICMP_ECHOREPLY = 0;
 ssize_t sendto(int, const void *, size_t, int, const struct sockaddr *, unsigned int);
 ssize_t recv(int, void *, size_t, int);
 struct iphdr {
@@ -425,155 +353,4 @@ if jit.os == "Linux" then
   -- NOTE: There's no librt.so symlink, so, specify the SOVER, but not the full path,
   --       in order to let the dynamic loader figure it out on its own (e.g.,  multilib).
   pcall(ffi.load, "rt.so.1", true)
-end
-
--- The clockid_t constants are not portable :/.
-if jit.os == "Linux" then
-  ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = 5;
-static const int CLOCK_MONOTONIC = 1;
-static const int CLOCK_MONOTONIC_COARSE = 6;
-static const int CLOCK_MONOTONIC_RAW = 4;
-static const int CLOCK_BOOTTIME = 7;
-static const int CLOCK_TAI = 11;
-static const int FIONREAD = 21531;
-]])
-elseif jit.os == "OSX" then
-  -- c.f., https://github.com/phracker/MacOSX-SDKs/blob/master/MacOSX10.12.sdk/usr/include/time.h
-  --[[
-    typedef enum {
-    _CLOCK_REALTIME             = 0,
-    _CLOCK_MONOTONIC            = 6,
-    _CLOCK_MONOTONIC_RAW        = 4,
-    _CLOCK_MONOTONIC_RAW_APPROX = 5,
-    _CLOCK_UPTIME_RAW           = 8,
-    _CLOCK_UPTIME_RAW_APPROX    = 9,
-    _CLOCK_PROCESS_CPUTIME_ID   = 12,
-    _CLOCK_THREAD_CPUTIME_ID    = 16
-    } clockid_t;
-    --]]
-  -- Portability notes:
-  -- Unlike on Linux, MONO ticks during sleep (which is technically the POSIX-compliant behavior).
-  -- CLOCK_UPTIME_* doesn't.
-  -- (e.g., macOS UPTIME == Linux MONO, and macOS MONO == Linux BOOTTIME)
-
-  -- NOTE: Requires macOS 10.12
-  ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = -1;
-static const int CLOCK_MONOTONIC = 6;
-static const int CLOCK_MONOTONIC_COARSE = 5;
-static const int CLOCK_MONOTONIC_RAW = 4;
-static const int CLOCK_BOOTTIME = -1;
-static const int CLOCK_TAI = -1;
-static const int FIONREAD = 1074030207;
-]])
-elseif jit.os == "BSD" then
-  -- OpenBSD
-  -- c.f., https://github.com/openbsd/src/blob/master/sys/sys/_time.h
-  --[[
-    #define CLOCK_REALTIME           0
-    #define CLOCK_PROCESS_CPUTIME_ID 2
-    #define CLOCK_MONOTONIC          3
-    #define CLOCK_THREAD_CPUTIME_ID  4
-    #define CLOCK_UPTIME             5
-    #define CLOCK_BOOTTIME           6
-    --]]
-  -- Portability notes:
-  -- OpenBSD UPTIME == Linux MONOTONIC, OpenBSD BOOTTIME == Linux BOOTTIME
-  -- (Meaning MONOTONIC starts ticking at an *undefined* positive value).
-
-  -- NetBSD
-  -- c.f., https://anonhg.netbsd.org/src/file/tip/sys/sys/time.h
-  --[[
-    #define CLOCK_REALTIME           0
-    #define CLOCK_VIRTUAL            1
-    #define CLOCK_PROF               2
-    #define CLOCK_MONOTONIC          3
-    #define CLOCK_THREAD_CPUTIME_ID  0x20000000
-    #define CLOCK_PROCESS_CPUTIME_ID 0x40000000
-    --]]
-
-  -- FreeBSD
-  -- c.f., https://github.com/freebsd/freebsd-src/blob/main/include/time.h
-  --[[
-    #define CLOCK_REALTIME           0
-    #define CLOCK_VIRTUAL            1
-    #define CLOCK_PROF               2
-    #define CLOCK_MONOTONIC          4
-    #define CLOCK_UPTIME             5  /* FreeBSD-specific. */
-    #define CLOCK_UPTIME_PRECISE     7  /* FreeBSD-specific. */
-    #define CLOCK_UPTIME_FAST        8  /* FreeBSD-specific. */
-    #define CLOCK_REALTIME_PRECISE   9  /* FreeBSD-specific. */
-    #define CLOCK_REALTIME_FAST      10 /* FreeBSD-specific. */
-    #define CLOCK_MONOTONIC_PRECISE  11 /* FreeBSD-specific. */
-    #define CLOCK_MONOTONIC_FAST     12 /* FreeBSD-specific. */
-    #define CLOCK_SECOND             13 /* FreeBSD-specific. */
-    #define CLOCK_THREAD_CPUTIME_ID  14
-    #define CLOCK_PROCESS_CPUTIME_ID 15
-    --]]
-  -- Portability notes:
-  -- FreeBSD UPTIME == Linux MONOTONIC
-  -- (I assume that, like on OpenBSD, this means MONOTONIC starts ticking at an *undefined* positive value).
-
-  -- So, here comes probey-time!
-  local C = ffi.C
-  local probe_ts = ffi.new("struct timespec")
-  if C.clock_getres(15, probe_ts) == 0 then
-    -- FreeBSD
-    ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = 10;
-static const int CLOCK_MONOTONIC = 4;
-static const int CLOCK_MONOTONIC_COARSE = 12;
-static const int CLOCK_MONOTONIC_RAW = 11;
-static const int CLOCK_BOOTTIME = -1;
-static const int CLOCK_TAI = -1;
-]])
-  elseif C.clock_getres(0x40000000, probe_ts) == 0 then
-    -- NetBSD
-    ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = -1;
-static const int CLOCK_MONOTONIC = 3;
-static const int CLOCK_MONOTONIC_COARSE = -1;
-static const int CLOCK_MONOTONIC_RAW = -1;
-static const int CLOCK_BOOTTIME = -1;
-static const int CLOCK_TAI = -1;
-]])
-  else
-    -- OpenBSD
-    ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = -1;
-static const int CLOCK_MONOTONIC = 3;
-static const int CLOCK_MONOTONIC_COARSE = -1;
-static const int CLOCK_MONOTONIC_RAW = -1;
-static const int CLOCK_BOOTTIME = 6;
-static const int CLOCK_TAI = -1;
-]])
-  end
-  probe_ts = nil --luacheck: ignore
-else
-  -- Assume minimal Linux compat on other OSes.
-
-  -- This holds true for Windows via mingw,
-  -- c.f., https://github.com/mirror/mingw-w64/blob/master/mingw-w64-libraries/winpthreads/include/pthread_time.h
-  --[[
-    #define CLOCK_REALTIME           0
-    #define CLOCK_MONOTONIC          1
-    #define CLOCK_PROCESS_CPUTIME_ID 2
-    #define CLOCK_THREAD_CPUTIME_ID  3
-    --]]
-
-  ffi.cdef([[
-static const int CLOCK_REALTIME = 0;
-static const int CLOCK_REALTIME_COARSE = -1;
-static const int CLOCK_MONOTONIC = 1;
-static const int CLOCK_MONOTONIC_COARSE = -1;
-static const int CLOCK_MONOTONIC_RAW = -1;
-static const int CLOCK_BOOTTIME = -1;
-static const int CLOCK_TAI = -1;
-]])
 end
