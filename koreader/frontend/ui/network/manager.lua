8e844d9222a8e923bf5f031611d3ad8724304a6e
Hzj_jie <hzj_jie@hotmail.com>
2025-09-07 00:54:17 -0700

    Slightly improve NetworkMgr:_setOnlineState
    issue #132

--- koreader/frontend/ui/network/manager.lua
+++ koreader/frontend/ui/network/manager.lua
@@ -182,10 +182,9 @@ function NetworkMgr:_queryOnlineState()
 end
 
 function NetworkMgr:_setOnlineState(new_state)
-  local last_state = self.was_online
-  self.was_online = new_state
-  if last_state ~= self.was_online then
-    if self.was_online then
+  if self.was_online ~= new_state then
+    self.was_online = new_state
+    if new_state then
       raiseNetworkEvent("Online")
     else
       raiseNetworkEvent("Offline")
