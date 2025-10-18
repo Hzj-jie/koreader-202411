f9fbaf9bbf7231aa00ee1fa3c385b47353daaa54
Hzj_jie <hzj_jie@hotmail.com>
2025-10-17 22:14:56 -0700

    Make avoid_flashing_ui default and avoid full refreshing
    issue #201
    issue #191
    issue #200

--- koreader/frontend/ui/elements/screen_eink_opt_menu_table.lua
+++ koreader/frontend/ui/elements/screen_eink_opt_menu_table.lua
@@ -18,10 +18,10 @@ local eink_settings_table = {
     {
       text = _("Avoid mandatory black flashes in UI"),
       checked_func = function()
-        return G_reader_settings:isTrue("avoid_flashing_ui")
+        return G_reader_settings:nilOrTrue("avoid_flashing_ui")
       end,
       callback = function()
-        G_reader_settings:flipNilOrFalse("avoid_flashing_ui")
+        G_reader_settings:flipFalse("avoid_flashing_ui")
       end,
     },
   },
