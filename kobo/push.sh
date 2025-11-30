d2b2b6a969dd95a4cd7e48db231a1590be811f39
Hzj_jie <hzj_jie@hotmail.com>
2025-11-30 11:18:49 -0800

    rsync works on kobo now
    fix #95

--- kobo/push.sh
+++ kobo/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export KOREADER_DIR=/mnt/onboard/.adds/koreader
-rsync --rsync-path=$KOREADER_DIR -acvLK --no-o --no-g --exclude=lns.sh . root@192.168.1.16:$KOREADER_DIR
+rsync --rsync-path=$KOREADER_DIR/rsync -acvLK --no-o --no-g --exclude=lns.sh . root@192.168.1.16:$KOREADER_DIR
