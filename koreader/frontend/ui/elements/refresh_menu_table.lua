47157b716abf19264a44290cf954f69c6497e678
Zijie He <hzj_jie@hotmail.com>
2025-12-15 10:25:33 -0800

    update about full refresh help_text

--- koreader/frontend/ui/elements/refresh_menu_table.lua
+++ koreader/frontend/ui/elements/refresh_menu_table.lua
@@ -11,7 +11,7 @@ return {
     callback = function()
       UIManager:show(InfoMessage:new({
         -- Need localization
-        text = _("Showing a new content on an E-ink screen usually does not fully clear the previous content and leaves some blur. It may be referred as E-ink shadow or ghosting. The blur needs a full refresh to be cleared cleanly. But a full refresh is slow and can be noticed as a blink.\nKOReader allows users deciding the frequence of the full refresh, i.e. more full refreshes for better display quality or less full refreshes for responsiveness.\nUsually you do not to adjust this configuration, the balanced setting should be good enough for most of the use cases."),
+        text = _("Showing a new content on an E-ink screen usually does not fully clear the previous content and leaves some blur. It may be referred as E-ink shadow or ghosting.\nThe blur needs a full refresh to be cleared cleanly. But a full refresh can be slow and trigger a flicker.\nKOReader allows users deciding the frequence of the full refresh, i.e. more full refreshes for better display quality or less full refreshes for responsiveness.\nUsually you do not to adjust this configuration, the balanced setting should be good enough for most of the use cases."),
       }))
     end,
   },
@@ -32,5 +32,6 @@ return {
   {
     -- Need localization
     text = _("High full refresh rate for better display quality"),
+    separator = true,
   },
 }
