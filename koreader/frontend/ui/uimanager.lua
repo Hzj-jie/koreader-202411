Hzj-jie/koreader-202411

commit 74c593649e756bf9ff5c313e6b625d5b085756d3

Prefer time.since

koreader/frontend/ui/uimanager.lua
@@ -397,7 +397,7 @@ function UIManager:debounce(seconds, immediate, action)
 
   local scheduled_action
   scheduled_action = function()
-    local passed_from_last_call = time.monotonic() - previous_call_at
+    local passed_from_last_call = (time.since(previous_call_at) > 0)
     if seconds > passed_from_last_call then
       self:scheduleIn(seconds - passed_from_last_call, scheduled_action)
       is_scheduled = true

koreader/frontend/apps/reader/modules/readerdictionary.lua
@@ -1054,7 +1054,7 @@ function ReaderDictionary:stardictLookup(
   if
     results
     and results.lookup_cancelled
-    and (time.monotonic() - self._lookup_start_time)
+    and time.since(self._lookup_start_time)
       <= self.quick_dismiss_before_delay
   then
     -- If interrupted quickly just after launch, don't display anything

koreader/frontend/apps/reader/modules/readerscrolling.lua
@@ -377,7 +377,7 @@ function ReaderScrolling:_setupAction()
 
       -- Initiate inertial scrolling (action=true), unless we should not
       if
-        time.monotonic() - self._last_manual_scroll_timev
+        time.since(self._last_manual_scroll_timev)
         >= self.pause_before_release_cancel_duration
       then
         -- but not if no finger move for 0.3s before finger up

koreader/frontend/ui/widget/htmlboxwidget.lua
@@ -274,7 +274,7 @@ function HtmlBoxWidget:onHoldReleaseText(callback, ges)
     return false
   end
 
-  local hold_duration = time.monotonic() - self.hold_start_time
+  local hold_duration = time.since(self.hold_start_time)
 
   local page = self.document:openPage(self.page_number)
   local lines = page:getPageText()

koreader/frontend/ui/widget/textboxwidget.lua
@@ -2276,7 +2276,7 @@ function TextBoxWidget:onHoldReleaseText(callback, ges)
     return false
   end
 
-  local hold_duration = time.monotonic() - self.hold_start_time
+  local hold_duration = time.since(self.hold_start_time)
 
   -- If page contains an image, check if Hold is on this image and deal
   -- with it directly

koreader/plugins/backgroundrunner.koplugin/main.lua
@@ -262,7 +262,7 @@ function BackgroundRunner:_execute()
           job.when = 1
         end
         should_execute = (
-          time.monotonic() - job.insert_time >= time.s(job.when)
+          time.since(job.insert_time) >= time.s(job.when)
         )
       else
         logger.warn("ignore negative job.when, ", _debugJobStr(job))

commit 1f40bdc1e47bf9286e597e6eee36733aebb84017

UIManager:debounce was broken by 74c593649e756bf9ff5c313e6b625d5b085756d3
fix #321

koreader/frontend/ui/uimanager.lua
@@ -397,7 +397,7 @@ function UIManager:debounce(seconds, immediate, action)
 
   local scheduled_action
   scheduled_action = function()
-    local passed_from_last_call = (time.since(previous_call_at) > 0)
+    local passed_from_last_call = time.since(previous_call_at)
     if seconds > passed_from_last_call then
       self:scheduleIn(seconds - passed_from_last_call, scheduled_action)
       is_scheduled = true