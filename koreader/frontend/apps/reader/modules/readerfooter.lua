0fa2d89b89fb697d6b5a5296cea4b491602be7d5
Zijie He <hzj_jie@hotmail.com>
2025-12-23 10:36:47 +0800

    Use of UIManager:scheduleWidgetRepaint in readerfooter is wrong

--- koreader/frontend/apps/reader/modules/readerfooter.lua
+++ koreader/frontend/apps/reader/modules/readerfooter.lua
@@ -2194,7 +2194,7 @@ function ReaderFooter:_repaint()
     -- NOTE: self.view.footer -> self ;).
 
     -- c.f., ReaderView:paintTo()
-    UIManager:scheduleWidgetRepaint(self.view.footer, 0, 0)
+    UIManager:scheduleWidgetRepaint(self.view.footer)
     -- We've painted it first to ensure self.footer_content.dimen is sane
     UIManager:setDirty(nil, "ui", self.footer_content.dimen)
   else
