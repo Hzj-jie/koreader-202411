9fe81a838c5195e514eaa2b0391bf19f427ac461
Zijie He <hzj_jie@hotmail.com>
2025-12-27 15:39:15 +0800

    UIManager:setDirty respects the input refreshMode if any

--- koreader/frontend/ui/uimanager.lua
+++ koreader/frontend/ui/uimanager.lua
@@ -438,12 +438,13 @@ function UIManager:setDirty(widget, refreshMode, region)
     table.insert(self._refresh_func_stack, function()
       local m, r, d = refreshMode()
       r = r or region
-      if widget ~= nil then
+      if widget ~= nil and type(widget) == "table" then
         r = r or widget.dimen
         -- Avoid treating false wrongly.
         if d == nil then
           d = widget.dithered
         end
+        widget._refreshMode = m or widget._refreshMode
       end
       self:scheduleRefresh(m, r, d)
     end)
