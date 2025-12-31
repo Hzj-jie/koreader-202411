5a9f9a3daf1b011fa1a239b193161de95a135410
Zijie He <hzj_jie@hotmail.com>
2025-12-31 23:32:18 +0800

    Run rsync in legacy/push.sh

--- legacy/push.sh
+++ legacy/push.sh
@@ -5,9 +5,7 @@ pushd ..
 ./update-git-rev.sh
 popd
 
-rm -rf /media/hzj_jie/Kindle/koreader.old/
-mv /media/hzj_jie/Kindle/koreader /media/hzj_jie/Kindle/koreader.old
-cp -rL . /media/hzj_jie/Kindle/koreader/
-cp -r --update=none /media/hzj_jie/Kindle/koreader.old/* /media/hzj_jie/Kindle/koreader/
+rsync -avL /media/hzj_jie/Kindle/koreader/ /media/hzj_jie/Kindle/koreader.old/
+rsync -avL . /media/hzj_jie/Kindle/koreader/
 
 git checkout ../koreader/git-rev
