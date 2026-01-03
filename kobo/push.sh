6d1e99ff888cd21162144b34bb345f63c24e0e67
Hzj_jie <hzj_jie@hotmail.com>
2026-01-02 21:10:59 -0800

    update-git-rev in kobo/push.sh

--- kobo/push.sh
+++ kobo/push.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+pushd ..
+./update-git-rev.sh
+popd
+
 export STORAGE_DIR=/mnt/onboard/.adds
 export KOREADER_DIR=$STORAGE_DIR/koreader
 rsync -e "ssh -o HostKeyAlgorithms=ecdsa-sha2-nistp256" --rsync-path=$KOREADER_DIR/rsync -acvLK --no-o --no-g --exclude=lns.sh . root@192.168.1.16:$KOREADER_DIR
