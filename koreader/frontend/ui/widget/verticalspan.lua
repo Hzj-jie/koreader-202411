522553ce15054077c915b8933b187d06178126c9
Zijie He <hzj_jie@hotmail.com>
2026-01-16 15:23:28 -0800

    VerticalSpan should have a height

--- koreader/frontend/ui/widget/verticalspan.lua
+++ koreader/frontend/ui/widget/verticalspan.lua
@@ -13,10 +13,6 @@ function VerticalSpan:init()
     logger.warn("FixMe: VerticalSpan should have a height rather than width")
     self.height = self.width
   end
-  if not self.height then
-    logger.warn("FixMe: VerticalSpan should have a height")
-    self.height = 0
-  end
 end
 
 return VerticalSpan
