Hzj-jie/koreader-202411

commit d6e97f259a336ebbe3fa75a90fe28e314c4f8ba5

All page turn behaviors respect page turn setup
fix #139

koreader/frontend/ui/widget/container/inputcontainer.lua
@@ -149,12 +149,7 @@ function InputContainer:registerTouchZones(zones)
       gs_range = GestureRange:new({
         ges = zone.ges,
         rate = zone.rate,
-        range = Geom:new({
-          x = screen_width * zone.screen_zone.ratio_x,
-          y = screen_height * zone.screen_zone.ratio_y,
-          w = screen_width * zone.screen_zone.ratio_w,
-          h = screen_height * zone.screen_zone.ratio_h,
-        }),
+        range = Geom:new({ x = 0, y = 0, w = screen_width, h = screen_height }):resize(zone.screen_zone),
       }),
     }
     self.touch_zone_dg:addNode(zone.id)
@@ -217,11 +212,7 @@ Updates touch zones based on new screen dimensions.
 ]]
 function InputContainer:updateTouchZonesOnScreenResize(new_screen_dimen)
   for _, tzone in ipairs(self._ordered_touch_zones) do
-    local range = tzone.gs_range.range
-    range.x = new_screen_dimen.w * tzone.def.screen_zone.ratio_x
-    range.y = new_screen_dimen.h * tzone.def.screen_zone.ratio_y
-    range.w = new_screen_dimen.w * tzone.def.screen_zone.ratio_w
-    range.h = new_screen_dimen.h * tzone.def.screen_zone.ratio_h
+    tzone.gs_range.range:resize(tzone.def.screen_zone)
   end
 end
 

koreader/frontend/apps/reader/modules/readerview.lua
@@ -1359,6 +1359,8 @@ function ReaderView:onToggleReadingOrder()
   return true
 end
 
+-- Should use only forward_zone, no point of using only part of the screen to
+-- turn pages.
 function ReaderView:getTapZones()
   local forward_zone, backward_zone
   local DTAP_ZONE_FORWARD = G_defaults:readSetting("DTAP_ZONE_FORWARD")
@@ -1426,7 +1428,9 @@ function ReaderView:getTapZones()
       }
     end
   end
-  if self.inverse_reading_order ~= BD.mirroredUILayout() then -- mirrored reading
+  -- Allow running without self.
+  -- Mirrored reading
+  if self and self.inverse_reading_order ~= BD.mirroredUILayout() then
     forward_zone.ratio_x = 1 - forward_zone.ratio_x - forward_zone.ratio_w
     backward_zone.ratio_x = 1 - backward_zone.ratio_x - backward_zone.ratio_w
   end

koreader/frontend/ui/geometry.lua
@@ -466,6 +466,22 @@ function Geom:isEmpty()
 end
 
 --[[--
+Resizes the Geom according to the ratio.
+]]
+function Geom:resize(ratio)
+  assert(ratio ~= nil)
+  local x = self.x + self.w * ratio.ratio_x
+  local y = self.y + self.h * ratio.ratio_y
+  local w = self.w * ratio.ratio_w
+  local h = self.h * ratio.ratio_h
+  self.x = x
+  self.y = y
+  self.w = w
+  self.h = h
+  return self
+end
+
+--[[--
 Returns a bounding box which encompasses all passed rectangles.
 @tparam Geom rectangles to encompass
 @treturn Geom bounding box or nil if no rectangles passed

koreader/frontend/ui/widget/dictquicklookup.lua
@@ -1202,10 +1202,12 @@ function DictQuickLookup:onTap(arg, ges_ev)
     -- processed for scrolling definition by ScrollTextWidget, which
     -- will pop it up for us here when it can't scroll anymore).
     -- This allow for continuous reading of results' definitions with tap.
-    if BD.flipIfMirroredUILayout(ges_ev.pos.x < Screen:getWidth() / 2) then
-      self:onReadPrevResult()
-    else
+    -- Ignore the second result / backward_zone & late initialization.
+    local forward_zone = require("apps/reader/modules/readerview"):getTapZones()
+    if BD.flipIfMirroredUILayout(ges_ev.pos:intersectWith(self.definition_widget.dimen:copy():resize(forward_zone))) then
       self:onReadNextResult()
+    else
+      self:onReadPrevResult()
     end
   end
   return true

koreader/frontend/ui/widget/pagebrowserwidget.lua
@@ -1605,10 +1605,12 @@ function PageBrowserWidget:onTap(arg, ges)
   end
   -- If tap on a blank area, handle as prev/next page, so people
   -- not friend with swipe can still move around
-  if BD.flipIfMirroredUILayout(ges.pos.x < Screen:getWidth() / 2) then
-    self:onScrollPageUp()
-  else
+  -- Ignore the second result / backward_zone & late initialization.
+  local forward_zone = require("apps/reader/modules/readerview"):getTapZones()
+  if BD.flipIfMirroredUILayout(ges.pos:intersectWith(self.dimen:copy():resize(forward_zone))) then
     self:onScrollPageDown()
+  else
+    self:onScrollPageUp()
   end
   return true
 end

koreader/frontend/ui/widget/scrollhtmlwidget.lua
@@ -199,10 +199,14 @@ function ScrollHtmlWidget:onScrollText(arg, ges)
 end
 
 function ScrollHtmlWidget:onTapScrollText(arg, ges)
-  if BD.flipIfMirroredUILayout(ges.pos.x < Screen:getWidth() / 2) then
-    return self:onScrollUp()
-  else
+    -- Ignore the second result / backward_zone & late initialization.
+    local forward_zone = require("apps/reader/modules/readerview"):getTapZones()
+  if BD.flipIfMirroredUILayout(
+ges.pos:intersectWith(self.dimen:copy():resize(forward_zone))
+    ) then
     return self:onScrollDown()
+  else
+    return self:onScrollUp()
   end
 end
 

koreader/frontend/ui/widget/scrolltextwidget.lua
@@ -320,10 +320,12 @@ function ScrollTextWidget:onTapScrollText(arg, ges)
     return false
   end
   -- same tests as done in TextBoxWidget:scrollUp/Down
-  if BD.flipIfMirroredUILayout(ges.pos.x < Screen:getWidth() / 2) then
-    return self:onScrollUp()
-  else
+    -- Ignore the second result / backward_zone & late initialization.
+    local forward_zone = require("apps/reader/modules/readerview"):getTapZones()
+  if BD.flipIfMirroredUILayout(ges.pos:intersectWith(self.dimen:copy():resize(forward_zone))) then
     return self:onScrollDown()
+  else
+    return self:onScrollUp()
   end
 end
 

commit 06ffbfcaa5a7045bf5927388e3a45e1e48e3d544

InputContainer:updateTouchZonesOnScreenResize was broken by https://github.com/Hzj-jie/koreader-202411/commit/d6e97f259a336ebbe3fa75a90fe28e314c4f8ba5
fix #250
fix #410

koreader/frontend/ui/widget/container/inputcontainer.lua
@@ -213,6 +213,7 @@ Updates touch zones based on new screen dimensions.
 ]]
 function InputContainer:updateTouchZonesOnScreenResize(new_screen_dimen)
   for _, tzone in ipairs(self._ordered_touch_zones) do
+    tzone.gs_range.range = new_screen_dimen:copy()
     tzone.gs_range.range:resize(tzone.def.screen_zone)
   end
 end