8598b67fcc9b8395428bd74e16ef00f5fac0aee1
zijiehe-google-com <zijiehe@google.com>
2026-06-04 13:42:10 -0700

    test: add unit test for MultiInputDialog onClose cleanup

---
+++ linux/spec/unit/multiinputdialog_spec.lua
@@ -0,0 +1,36 @@
+describe("MultiInputDialog widget", function()
+    local MultiInputDialog, InputText, InputDialog
+    setup(function()
+        require("commonrequire")
+        MultiInputDialog = require("ui/widget/multiinputdialog")
+        InputText = require("ui/widget/inputtext")
+        InputDialog = require("ui/widget/inputdialog")
+    end)
+
+    it("should close all input fields when closed", function()
+        local Event = require("ui/event")
+
+        local calls = 0
+        local original_onClose = InputText.onClose
+        InputText.onClose = function(self)
+            calls = calls + 1
+            return original_onClose(self)
+        end
+
+        local dialog = MultiInputDialog:new({
+            title = "Test",
+            fields = {
+                { text = "1" },
+                { text = "2" },
+                { text = "3" },
+            },
+        })
+
+        dialog:broadcastEvent(Event:new("Close"))
+
+        -- 1 (measurement dummy) + 1 (discarded main dummy) + 3 (fields) = 5
+        assert.are.equal(5, calls)
+
+        InputText.onClose = original_onClose
+    end)
+end)
