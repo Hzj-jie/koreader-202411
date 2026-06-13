f7ca0a57991878a85d1705ef78e0e1cf2cdefb4f
zijiehe-google-com <zijiehe@google.com>
2026-06-12 23:27:15 -0700

    L10n: Fix Lua format validation and restore standard multiline formatting

--- koreader/plugins/vocabbuilder.koplugin/main.lua
+++ koreader/plugins/vocabbuilder.koplugin/main.lua
@@ -1047,12 +1047,14 @@ function VocabItemWidget:getTimeSinceDue()
 
   local rounding = elapsed > 0 and math.floor or math.ceil
   if abs < 60 then
+    -- xgettext: no-lua-format
     readable_time = T(C_("Time", "%1s"), abs)
   elseif abs < 3600 then
     readable_time = T(C_("Time", "%1m"), rounding(abs / 60))
   elseif abs < 3600 * 24 then
     readable_time = T(C_("Time", "%1h"), rounding(abs / 3600))
   elseif abs < 3600 * 24 * 30 then
+    -- xgettext: no-lua-format
     readable_time = T(C_("Time", "%1d"), rounding(abs / 3600 / 24))
   elseif abs < 3600 * 24 * 365 then
     readable_time = T(C_("Time", "%1 mo."), rounding(abs / 3600 / 24 / 3) / 10)
