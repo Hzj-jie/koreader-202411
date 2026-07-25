42dd0f5edee9a8aebd38a02e90e8f25f7e328fe4
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:40:11 -0700

    test(waveform_level): add unit test coverage

---
+++ linux/spec/unit/waveform_level_spec.lua
@@ -0,0 +1,12 @@
+describe("WaveformLevel element", function()
+  local WaveformLevel
+
+  setup(function()
+    require("commonrequire")
+    WaveformLevel = require("ui/elements/waveform_level")
+  end)
+
+  it("should return waveform level menu table or nil depending on device", function()
+    assert.is_true(WaveformLevel == nil or type(WaveformLevel) == "table")
+  end)
+end)
