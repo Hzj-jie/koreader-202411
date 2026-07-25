d5229e2e7013438d6bf354a1f277185e18ac6307
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:37:23 -0700

    test(readerwikipedia): add unit test coverage

---
+++ linux/spec/unit/readerwikipedia_spec.lua
@@ -0,0 +1,25 @@
+describe("ReaderWikipedia module", function()
+  local ReaderWikipedia, DocumentRegistry, ReaderUI, Screen
+
+  setup(function()
+    require("commonrequire")
+    ReaderWikipedia = require("apps/reader/modules/readerwikipedia")
+    DocumentRegistry = require("document/documentregistry")
+    ReaderUI = require("apps/reader/readerui")
+    Screen = require("device").screen
+  end)
+
+  it("should initialize wikipedia module", function()
+    local sample_epub = "spec/front/unit/data/leaves.epub"
+    local readerui = ReaderUI:new({
+      dimen = Screen:getSize(),
+      document = DocumentRegistry:openDocument(sample_epub),
+    })
+
+    local wikipedia = readerui.wikipedia
+    assert.is_table(wikipedia)
+
+    readerui:onExit()
+    readerui:onClose()
+  end)
+end)
