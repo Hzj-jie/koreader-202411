95b9a66e7e4d203d503befe6a8fcc2422e1bcd57
zijiehe-google-com <zijiehe@google.com>
2026-07-25 10:45:18 -0700

    test(wikipedia): add unit test coverage

--- linux/spec/unit/wikipedia_spec.lua
+++ linux/spec/unit/wikipedia_spec.lua
@@ -11,4 +11,19 @@ describe("Wikipedia module", function()
     assert.is.same(expected_server_default, Wikipedia:getWikiServer())
     assert.is.same(expected_server_nl, Wikipedia:getWikiServer("nl"))
   end)
+
+  it("should manage trap widget", function()
+    local dummy_trap = { name = "dummy" }
+    Wikipedia:setTrapWidget(dummy_trap)
+    assert.is.same(dummy_trap, Wikipedia.trap_widget)
+    Wikipedia:resetTrapWidget()
+    assert.is_nil(Wikipedia.trap_widget)
+  end)
+
+  it("should prettify plain text headers and whitespace", function()
+    local input = "\n\n== Section Header ==\n\nSome paragraph text.\n\n\n"
+    local prettified = Wikipedia:prettifyText(input)
+    assert.truthy(prettified:find("Section Header"))
+    assert.is_not.same(input, prettified)
+  end)
 end)
