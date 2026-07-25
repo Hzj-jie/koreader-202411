16f80137aca32742613c3ae3e40e9bc264991eb9
zijiehe-google-com <zijiehe@google.com>
2026-07-25 14:44:20 -0700

    test(checkers_board): add unit test coverage

---
+++ linux/spec/unit/checkers_board_spec.lua
@@ -0,0 +1,20 @@
+describe("Checkers Board widget module", function()
+  local Board, Game
+
+  setup(function()
+    require("commonrequire")
+    Board = require("plugins/checkers.koplugin/board")
+    Game = require("plugins/checkers.koplugin/game")
+  end)
+
+  it("should initialize Checkers Board widget instance", function()
+    local game = Game:new()
+    local board = Board:new({
+      game = game,
+      width = 400,
+      height = 400,
+    })
+
+    assert.is_table(board)
+  end)
+end)
