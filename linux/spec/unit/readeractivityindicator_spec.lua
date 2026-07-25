4d712e21bf9a17a4ac273179a15266203bfddd6c
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:38:40 -0700

    test(readeractivityindicator): add unit test coverage

---
+++ linux/spec/unit/readeractivityindicator_spec.lua
@@ -0,0 +1,13 @@
+describe("ReaderActivityIndicator module", function()
+  local ReaderActivityIndicator
+
+  setup(function()
+    require("commonrequire")
+    ReaderActivityIndicator = require("apps/reader/modules/readeractivityindicator")
+  end)
+
+  it("should initialize activity indicator module stub", function()
+    assert.is_table(ReaderActivityIndicator)
+    assert.is_boolean(ReaderActivityIndicator:isStub())
+  end)
+end)
