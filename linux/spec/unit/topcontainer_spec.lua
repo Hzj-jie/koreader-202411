393b39a597f74d593dccd1b227dd7037a410df2e
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:40:26 -0700

    test(topcontainer): add unit test coverage

---
+++ linux/spec/unit/topcontainer_spec.lua
@@ -0,0 +1,20 @@
+describe("TopContainer widget", function()
+  local TopContainer, Geom, Widget
+
+  setup(function()
+    require("commonrequire")
+    TopContainer = require("ui/widget/container/topcontainer")
+    Geom = require("ui/geometry")
+    Widget = require("ui/widget/widget")
+  end)
+
+  it("should initialize top container", function()
+    local inner_widget = Widget:new({ dimen = Geom:new({ x = 0, y = 0, w = 50, h = 50 }) })
+    local container = TopContainer:new({
+      dimen = Geom:new({ x = 0, y = 0, w = 100, h = 100 }),
+      [1] = inner_widget,
+    })
+
+    assert.is_table(container)
+  end)
+end)
