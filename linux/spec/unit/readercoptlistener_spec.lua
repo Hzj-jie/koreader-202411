d2ebb7f728e84b943942db7e7d24dc67b0657033
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:37:36 -0700

    test(readercoptlistener): add unit test coverage

---
+++ linux/spec/unit/readercoptlistener_spec.lua
@@ -0,0 +1,25 @@
+describe("ReaderCoptListener module", function()
+  local ReaderCoptListener, DocumentRegistry, ReaderUI, Screen
+
+  setup(function()
+    require("commonrequire")
+    ReaderCoptListener = require("apps/reader/modules/readercoptlistener")
+    DocumentRegistry = require("document/documentregistry")
+    ReaderUI = require("apps/reader/readerui")
+    Screen = require("device").screen
+  end)
+
+  it("should instantiate copt listener module", function()
+    local sample_epub = "spec/front/unit/data/leaves.epub"
+    local doc = DocumentRegistry:openDocument(sample_epub)
+    local listener = ReaderCoptListener:new({
+      document = doc,
+      view = {},
+    })
+
+    assert.is_table(listener)
+    assert.is_table(listener.additional_header_content)
+
+    doc:close()
+  end)
+end)
