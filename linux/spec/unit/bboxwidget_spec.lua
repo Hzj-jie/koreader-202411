2a2e6c3ba56e3d168c031bb36e47a034276af547
zijiehe-google-com <zijiehe@google.com>
2026-07-25 14:54:11 -0700

    test(bboxwidget): add unit test coverage for bounding box widget

--- linux/spec/unit/bboxwidget_spec.lua
+++ linux/spec/unit/bboxwidget_spec.lua
@@ -1,13 +1,37 @@
-describe("BBoxWidget widget", function()
-  local BBoxWidget
+describe("BBoxWidget widget module", function()
+  local BBoxWidget, Geom
 
   setup(function()
     require("commonrequire")
     BBoxWidget = require("ui/widget/bboxwidget")
+    Geom = require("ui/geometry")
   end)
 
-  it("should initialize BBoxWidget class", function()
-    assert.is_table(BBoxWidget)
-    assert.is_function(BBoxWidget.new)
+  it("should initialize BBoxWidget instance", function()
+    local mock_document = {
+      getPageBBox = function()
+        return { x = 0, y = 0, w = 100, h = 100 }
+      end,
+      getPageDimensions = function()
+        return { w = 100, h = 100 }
+      end,
+    }
+    local mock_view = {
+      state = { page = 1 },
+      dimen = Geom:new({ w = 600, h = 800 }),
+      pageToScreenBBox = function()
+        return { x = 0, y = 0, w = 100, h = 100 }
+      end,
+      screenToPageBBox = function()
+        return { x = 0, y = 0, w = 100, h = 100 }
+      end,
+    }
+
+    local widget = BBoxWidget:new({
+      document = mock_document,
+      view = mock_view,
+    })
+    assert.is_table(widget)
+    assert.is_table(widget.page_bbox)
   end)
 end)
