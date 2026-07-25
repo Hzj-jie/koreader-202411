054298c78c29245d63568cd3bc35ef839c5e67a1
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:40:18 -0700

    test(bottomcontainer): add unit test coverage

---
+++ linux/spec/unit/bottomcontainer_spec.lua
@@ -0,0 +1,20 @@
+describe("BottomContainer widget", function()
+  local BottomContainer, Geom, Widget
+
+  setup(function()
+    require("commonrequire")
+    BottomContainer = require("ui/widget/container/bottomcontainer")
+    Geom = require("ui/geometry")
+    Widget = require("ui/widget/widget")
+  end)
+
+  it("should initialize bottom container", function()
+    local inner_widget = Widget:new({ dimen = Geom:new({ x = 0, y = 0, w = 50, h = 50 }) })
+    local container = BottomContainer:new({
+      dimen = Geom:new({ x = 0, y = 0, w = 100, h = 100 }),
+      [1] = inner_widget,
+    })
+
+    assert.is_table(container)
+  end)
+end)
