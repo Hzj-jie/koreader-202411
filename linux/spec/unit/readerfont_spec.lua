b1362d9dd963833c2d027f03bb5a47daace00624
zijiehe-google-com <zijiehe@google.com>
2026-07-25 15:20:42 -0700

    test(readerfont): add unit test coverage

--- linux/spec/unit/readerfont_spec.lua
+++ linux/spec/unit/readerfont_spec.lua
@@ -1,12 +1,27 @@
-describe("ReaderFont module", function()
-  local ReaderFont
+local ReaderFont
 
+describe("ReaderFont module", function()
   setup(function()
     require("commonrequire")
     ReaderFont = require("apps/reader/modules/readerfont")
   end)
 
-  it("should initialize ReaderFont class", function()
-    assert.is_table(ReaderFont)
-  end)
+  it(
+    "should initialize ReaderFont instance and register to main menu",
+    function()
+      local font_mod = ReaderFont:new({
+        font_face = "Noto Serif",
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+      })
+      assert.is_table(font_mod)
+
+      local menu_items = {}
+      font_mod:addToMainMenu(menu_items)
+      assert.is_table(menu_items.change_font)
+    end
+  )
 end)
