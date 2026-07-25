a84a525d9222994d13711c2fd3fcc8aa032cfb45
zijiehe-google-com <zijiehe@google.com>
2026-07-25 11:37:46 -0700

    test(readerscrolling): add unit test coverage

---
+++ linux/spec/unit/readerscrolling_spec.lua
@@ -0,0 +1,25 @@
+describe("ReaderScrolling module", function()
+  local ReaderScrolling, DocumentRegistry, ReaderUI, Screen
+
+  setup(function()
+    require("commonrequire")
+    ReaderScrolling = require("apps/reader/modules/readerscrolling")
+    DocumentRegistry = require("document/documentregistry")
+    ReaderUI = require("apps/reader/readerui")
+    Screen = require("device").screen
+  end)
+
+  it("should initialize scrolling module", function()
+    local sample_pdf = "spec/front/unit/data/sample.pdf"
+    local readerui = ReaderUI:new({
+      dimen = Screen:getSize(),
+      document = DocumentRegistry:openDocument(sample_pdf),
+    })
+
+    local scrolling = readerui.scrolling
+    assert.is_table(scrolling)
+
+    readerui:onExit()
+    readerui:onClose()
+  end)
+end)
