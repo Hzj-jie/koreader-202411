1bdf58c0456f18eda7b465742c4f5df566675f92
zijiehe-google-com <zijiehe@google.com>
2026-07-25 22:54:32 -0700

    test(readerfont): add unit test coverage

--- linux/spec/unit/readerfont_spec.lua
+++ linux/spec/unit/readerfont_spec.lua
@@ -1,27 +1,72 @@
-local ReaderFont
-
 describe("ReaderFont module", function()
+  local ReaderFont, UIManager, Font
+
   setup(function()
     require("commonrequire")
+    package.unloadAll()
+    require("document/canvascontext"):init(require("device"))
+    UIManager = require("ui/uimanager")
+    Font = require("ui/font")
     ReaderFont = require("apps/reader/modules/readerfont")
   end)
 
-  it(
-    "should initialize ReaderFont instance and register to main menu",
-    function()
+  local function create_mock_ui()
+    return {
+      menu = {
+        registerToMainMenu = spy.new(function() end),
+      },
+      document = {
+        fallback_fonts = { "Noto Sans" },
+        info = {
+          has_crengine = true,
+        },
+      },
+    }
+  end
+
+  describe("Initialization & Main Menu", function()
+    it(
+      "should initialize ReaderFont instance and register to main menu",
+      function()
+        local mock_ui = create_mock_ui()
+        local font_mod = ReaderFont:new({
+          font_face = "Noto Serif",
+          ui = mock_ui,
+        })
+        assert.is_table(font_mod)
+        assert.spy(mock_ui.menu.registerToMainMenu).was_called()
+
+        local menu_items = {}
+        font_mod:addToMainMenu(menu_items)
+        assert.is_table(menu_items.change_font)
+      end
+    )
+  end)
+
+  describe("Font Face & Size Operations", function()
+    it("should handle font face setting", function()
+      local mock_ui = create_mock_ui()
+      local font_mod = ReaderFont:new({
+        font_face = "FreeSerif",
+        ui = mock_ui,
+      })
+
+      assert.are.equal(font_mod.font_face, "FreeSerif")
+    end)
+
+    it("should provide font settings submenus", function()
+      local mock_ui = create_mock_ui()
       local font_mod = ReaderFont:new({
         font_face = "Noto Serif",
-        ui = {
-          menu = {
-            registerToMainMenu = function() end,
-          },
-        },
+        ui = mock_ui,
+        font_family_fonts = {},
       })
-      assert.is_table(font_mod)
 
-      local menu_items = {}
-      font_mod:addToMainMenu(menu_items)
-      assert.is_table(menu_items.change_font)
-    end
-  )
+      if type(font_mod.getFontSettingsTable) == "function" then
+        local settings_table = font_mod:getFontSettingsTable()
+        assert.is_table(settings_table)
+        assert.truthy(#settings_table > 0)
+      end
+    end)
+  end)
 end)
