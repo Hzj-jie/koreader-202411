d94f36bbedb9fdf4bdb24ba9615b721d2059ed97
zijiehe-google-com <zijiehe@google.com>
2026-07-26 10:59:39 -0700

    test(sokoban_main): add menu and dispatcher unit test coverage

--- linux/spec/unit/sokoban_main_spec.lua
+++ linux/spec/unit/sokoban_main_spec.lua
@@ -18,4 +18,34 @@ describe("Sokoban main plugin module", function()
 
     assert.is_table(plugin)
   end)
+
+  describe("Menu & Dispatcher Integration", function()
+    it("should populate main menu items", function()
+      local plugin = Sokoban:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/sokoban.koplugin",
+      })
+      local menu_items = {}
+      plugin:addToMainMenu(menu_items)
+      assert.is_table(menu_items.sokoban)
+    end)
+
+    it("should register dispatcher actions", function()
+      local plugin = Sokoban:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/sokoban.koplugin",
+      })
+      if type(plugin.onDispatcherRegisterActions) == "function" then
+        plugin:onDispatcherRegisterActions()
+      end
+    end)
+  end)
 end)
