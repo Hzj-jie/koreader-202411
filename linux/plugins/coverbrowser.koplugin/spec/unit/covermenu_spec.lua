9b042956ec84169c82228475daaf18961af456bf
zijiehe-google-com <zijiehe@google.com>
2026-07-26 11:01:31 -0700

    test(coverbrowser_covermenu): add unit test coverage

---
+++ linux/plugins/coverbrowser.koplugin/spec/unit/covermenu_spec.lua
@@ -0,0 +1,18 @@
+describe("Coverbrowser CoverMenu module", function()
+  local CoverMenu
+
+  setup(function()
+    require("commonrequire")
+    package.unloadAll()
+    require("document/canvascontext"):init(require("device"))
+
+    CoverMenu = require("plugins/coverbrowser.koplugin/covermenu")
+  end)
+
+  describe("Initialization & Defaults", function()
+    it("should expose CoverMenu helper methods table", function()
+      assert.is_table(CoverMenu)
+      assert.is_function(CoverMenu.updateCache)
+    end)
+  end)
+end)
