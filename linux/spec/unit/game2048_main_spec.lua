f896288917153105c7856f4613c940ae56fa1a84
zijiehe-google-com <zijiehe@google.com>
2026-07-26 10:37:19 -0700

    test(game2048): add main unit test coverage

--- linux/spec/unit/game2048_main_spec.lua
+++ linux/spec/unit/game2048_main_spec.lua
@@ -18,4 +18,34 @@ describe("Game2048 plugin main module", function()
 
     assert.is_table(plugin)
   end)
+
+  describe("Menu & Dispatcher Integration", function()
+    it("should populate main menu items", function()
+      local plugin = Game2048:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/game2048.koplugin",
+      })
+      local menu_items = {}
+      plugin:addToMainMenu(menu_items)
+      assert.is_table(menu_items.game2048)
+    end)
+
+    it("should register dispatcher actions", function()
+      local plugin = Game2048:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/game2048.koplugin",
+      })
+      if type(plugin.onDispatcherRegisterActions) == "function" then
+        plugin:onDispatcherRegisterActions()
+      end
+    end)
+  end)
 end)
