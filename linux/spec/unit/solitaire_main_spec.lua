ed133556429029852b2d78f67f176d93112da6c9
zijiehe-google-com <zijiehe@google.com>
2026-07-26 11:02:58 -0700

    test(solitaire_main): add menu and dispatcher unit test coverage

--- linux/spec/unit/solitaire_main_spec.lua
+++ linux/spec/unit/solitaire_main_spec.lua
@@ -18,4 +18,34 @@ describe("Solitaire main plugin module", function()
 
     assert.is_table(plugin)
   end)
+
+  describe("Menu & Dispatcher Integration", function()
+    it("should populate main menu items", function()
+      local plugin = Solitaire:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/solitaire.koplugin",
+      })
+      local menu_items = {}
+      plugin:addToMainMenu(menu_items)
+      assert.is_table(menu_items.solitaire)
+    end)
+
+    it("should register dispatcher actions", function()
+      local plugin = Solitaire:new({
+        ui = {
+          menu = {
+            registerToMainMenu = function() end,
+          },
+        },
+        path = "plugins/solitaire.koplugin",
+      })
+      if type(plugin.onDispatcherRegisterActions) == "function" then
+        plugin:onDispatcherRegisterActions()
+      end
+    end)
+  end)
 end)
