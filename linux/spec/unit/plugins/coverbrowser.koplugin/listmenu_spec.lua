f5cd7cbd235fb8d3dae94ddb4faa9e52517e95d7
zijiehe-google-com <zijiehe@google.com>
2026-07-30 22:04:42 -0700

    test(spec): expand unit test coverage for listmenu module in coverbrowser

--- linux/spec/unit/plugins/coverbrowser.koplugin/listmenu_spec.lua
+++ linux/spec/unit/plugins/coverbrowser.koplugin/listmenu_spec.lua
@@ -1,17 +1,43 @@
 describe("Coverbrowser ListMenu module", function()
-  local ListMenu
+  local ListMenu, Geom
 
   setup(function()
     require("commonrequire")
     package.unloadAll()
     require("document/canvascontext"):init(require("device"))
 
+    Geom = require("ui/geometry")
     ListMenu = require("plugins/coverbrowser.koplugin/listmenu")
   end)
 
-  describe("Initialization & Defaults", function()
-    it("should expose ListMenu class table", function()
+  describe("Initialization & Mixin", function()
+    it("should expose ListMenu methods table", function()
       assert.is_table(ListMenu)
+      assert.is_function(ListMenu._recalculateDimen)
+    end)
+
+    it("should allow mixing ListMenu methods into a menu object", function()
+      local mock_menu = {
+        item_table = {
+          { text = "Book 1", path = "/tmp/book1.epub" },
+        },
+        page = 1,
+        perpage = 1,
+        width = 400,
+        height = 600,
+        item_group = {},
+        layout = {},
+        items_to_update = {},
+        path_items = {},
+        inner_dimen = Geom:new({ x = 0, y = 0, w = 400, h = 600 }),
+        item_dimen = Geom:new({ x = 0, y = 0, w = 400, h = 50 }),
+      }
+      setmetatable(mock_menu, { __index = ListMenu })
+
+      if type(mock_menu._recalculateDimen) == "function" then
+        mock_menu:_recalculateDimen()
+        assert.is_boolean(mock_menu.portrait_mode)
+      end
     end)
   end)
 end)
