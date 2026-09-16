52e69fa0859ae8ae683d2c8df52b0053f7c28102
zijiehe-google-com <zijiehe@google.com>
2026-09-16 13:53:08 -0700

    fix(backgroundrunner): default catch_exception to true to avoid crash (fix #494)

--- koreader/plugins/backgroundrunner.koplugin/main.lua
+++ koreader/plugins/backgroundrunner.koplugin/main.lua
@@ -61,7 +61,7 @@ local util = require("util")
 --   nil: ignore.
 --
 -- catch_exception: boolean
---   allow capturing the exception instead of crashing. Default false.
+--   allow capturing the exception instead of crashing. Default true.
 --
 -- If a job does not contain enough information, it will be ignored.
 --
@@ -105,6 +105,7 @@ local function _clone(job)
   result.executable = job.executable
   result.callback = job.callback
   result.environment = job.environment
+  result.catch_exception = job.catch_exception
   result.insert_time = time.monotonic()
   return result
 end
@@ -201,7 +202,7 @@ function BackgroundRunner:_executeJob(job)
       job.result = 1
       job.exception = err
       -- trigger the exception at the end to preserve the logs.
-      assert(job.catch_exception == true)
+      assert(job.catch_exception ~= false, err)
     end
     job.end_time = time.monotonic()
     self:_finishJob(job)
